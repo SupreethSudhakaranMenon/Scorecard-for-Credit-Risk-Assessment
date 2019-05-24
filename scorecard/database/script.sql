a5d6d997e0c434c5fe58b5f3a71a4a5778d99d72
root <supreethsudhakaranmenon14@gmail.com>
2019-05-24 23:25:50 +0530

    Add Database Table

--- scorecard/database/script.sql
+++ scorecard/database/script.sql
@@ -1,29 +1,11 @@
 CREATE DATABASE  IF NOT EXISTS `springbootdb` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `springbootdb`;
--- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
---
--- Host: 127.0.0.1    Database: springbootdb
--- ------------------------------------------------------
--- Server version	5.7.15
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
 -- Table structure for table `product`
---
+
 
 DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 
 
 
@@ -32,7 +14,7 @@ DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `version` varchar(512) CHARACTER SET utf8 DEFAULT NULL,
-  `characteristic` varchar(512) CHARACTER SET utf8 DEFAULT NULL,
+  `characteristic` varchar(512) CHARACTER SET utf8 DEFAULT NULL, 
   `value` varchar(20) DEFAULT NULL,
   `data` varchar(20) DEFAULT NULL,
   `category` varchar(20) DEFAULT NULL,
@@ -46,6 +28,7 @@ CREATE TABLE `configuration` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`version` varchar(512) CHARACTER SET utf8 DEFAULT NULL,
 	`product` varchar(20) DEFAULT NULL,
+	`colour` varchar(20) DEFAULT NULL,
 	`category` varchar(20) DEFAULT NULL,
 	`characteristic` varchar(20) DEFAULT NULL,
 	`hello` varchar(20) DEFAULT NULL,
@@ -58,15 +41,14 @@ CREATE TABLE `configuration` (
 /* hola is used for to */
 
 
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+CREATE TABLE `criteria` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`version` varchar(512) CHARACTER SET utf8 DEFAULT NULL,
+	`datasource` varchar(20) DEFAULT NULL,
+	`key` varchar(20) DEFAULT NULL,
+	`statistical` char(1) NOT NULL,
+	PRIMARY KEY (`id`)
+)
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 
