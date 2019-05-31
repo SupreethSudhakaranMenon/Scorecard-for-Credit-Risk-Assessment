85fd38a9d0f0f43db1147d2683d14152cdfcc30e
root <supreethsudhakaranmenon14@gmail.com>
2019-05-31 12:15:34 +0530

    Add mainJavafile

---
+++ scorecard/src/main/java/com/ensat/entities/Scorecard.java
@@ -0,0 +1,42 @@
+package com.ensat.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public class Scorecard {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Version
+	private Integer version;
+	
+	private String loan_id;
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getVersion() {
+		return version;
+	}
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+	public String getLoan_id() {
+		return loan_id;
+	}
+
+	public void setLoan_id(String loan_id) {
+		this.loan_id = loan_id;
+	}
+	
+}
