4cac53dcbbe7115289063e98624202ad25f8a7bc
root <supreethsudhakaranmenon14@gmail.com>
2019-05-31 12:20:01 +0530

    Add Repositoryfile

---
+++ scorecard/src/main/java/com/ensat/repositories/ScorecardRepository.java
@@ -0,0 +1,8 @@
+package com.ensat.repositories;
+
+import com.ensat.entities.Scorecard;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ScorecardRepository extends CrudRepository< Scorecard , Integer >{
+
+}
