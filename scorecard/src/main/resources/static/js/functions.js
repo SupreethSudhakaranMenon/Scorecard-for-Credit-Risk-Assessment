64bf42a0ea8b6a2b0171b91a26506081f20cfd57
root <supreethsudhakaranmenon14@gmail.com>
2019-06-07 18:10:27 +0530

    Add JSON request

--- scorecard/src/main/resources/static/js/functions.js
+++ scorecard/src/main/resources/static/js/functions.js
@@ -3,3 +3,10 @@ function conf()
 	alert("The Details have been submitted");
 	
 }
+
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET','https://supreethsudhakaranmenon.github.io/details.json');
+ourRequest.onload = function() {
+	console.log(ourRequest.responseText);
+};
+ourRequest.send();
